c84eaf6de949778f6f8f4d56b01aa029ad839ec0
Eric Eilebrecht <eric@certora.com>
2023-10-16 11:45:27 -0700

    Publication logic (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,22 @@
+import org.gradle.api.tasks.*
 import org.gradle.kotlin.dsl.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
 
 plugins {
 	kotlin("jvm")
 	kotlin("plugin.serialization")
 	id("io.github.detekt.gradle.compiler-plugin")
     id("java-library")
+	id("maven-publish")
 	id("com.palantir.git-version") version "3.0.0"
 }
 
 subprojects {
 	apply(plugin = "java-library")
 	apply(plugin = "com.palantir.git-version")
-
-	val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
-	val details = versionDetails()
+	apply(plugin = "maven-publish")
 	
-	if (project.hasProperty("release")) {
-		version = details.lastTag
-	} else {
-		version = "${details.lastTag}-SNAPSHOT"
-	}
-
 	repositories {
 		mavenCentral()
 	}
@@ -43,4 +38,45 @@ subprojects {
 			freeCompilerArgs += "-Xcontext-receivers"
 		}
 	}
+
+
+	publishing {
+		publications {
+			repositories {
+				maven {
+					name = "GitHubPackages"
+					url = URI("https://maven.pkg.github.com/Certora/collections")
+					credentials {
+						username = System.getenv("GITHUB_ACTOR")
+						password = System.getenv("GITHUB_TOKEN")
+					}
+				}
+			}
+
+			withType<MavenPublication> {
+				version = if (project.hasProperty("release")) {
+					val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+					val details = versionDetails()
+					"${details.lastTag}"
+				} else {
+					"${project.version}-SNAPSHOT"
+				}
+
+				pom {
+					licenses {
+						license {
+							name.set("MIT License")
+							url.set("https://github.com/Certora/collections/blob/4bc9da2c8197aea0ed3ad8b32b5a3dbcd69e725e/LICENSE")
+						}
+					}
+					scm {
+						connection.set("scm:git:https://github.com/Certora/collections.git")
+						developerConnection.set("scm:git:ssh://github.com/Certora/collections.git")
+						url.set("https://github.com/Certora/collections/")
+					}
+				}            
+			}
+		}
+	}
+
 }
--- collect/build.gradle.kts
+++ collect/build.gradle.kts
@@ -1,6 +1,5 @@
 import org.gradle.api.tasks.*
 import org.gradle.kotlin.dsl.*
-import java.net.URI
 
 plugins {
 	kotlin("jvm")
@@ -20,17 +19,6 @@ detekt {
 
 publishing {
     publications {
-		repositories {
-			maven {
-				name = "GitHubPackages"
-				url = URI("https://maven.pkg.github.com/Certora/collections")
-				credentials {
-					username = System.getenv("GITHUB_ACTOR")
-					password = System.getenv("GITHUB_TOKEN")
-				}
-			}
-		}
-
         create<MavenPublication>("certora-collections") {
             from(components["java"])
 
@@ -38,18 +26,7 @@ publishing {
                 name.set("Certrora Collections")
                 description.set("Efficient collection types for Kotlin")
                 url.set("http://www.github.com/Certora/collections")
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://github.com/Certora/collections/blob/4bc9da2c8197aea0ed3ad8b32b5a3dbcd69e725e/LICENSE")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://github.com/Certora/collections.git")
-                    developerConnection.set("scm:git:ssh://github.com/Certora/collections.git")
-                    url.set("https://github.com/Certora/collections/")
-                }
-            }            
+            }
         }
     }
 }
