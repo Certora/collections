7a1d54f5a82e2bf0c7e64af141643c63311414aa
Eric Eilebrecht <eric@certora.com>
2023-10-25 21:50:28 -0700

    graphs and stuff

--- benchmarks/src/main/kotlin/benchmarks/Sizes.kt
+++ benchmarks/src/main/kotlin/benchmarks/Sizes.kt
@@ -16,46 +16,21 @@ data class HashableSizeKey(val value: Int)
 object DummyValue
 
 
-@Serializable
-data class MapSizes(
+data class Maps(
     val scenario: String,
-    val size: Int,
-    val hashMap: Long,
-    val persistentHashMap: Long,
-    val hashTreapMap: Long,
-    val sortedTreapMap: Long
+    val maps: Map<String, Map<Any, Any>>,
+    val scenarioSize: Int = maps.values.first().size
+)
+
+data class Sets(
+    val scenario: String,
+    val sets: Map<String, Set<Any>>,
+    val scenarioSize: Int = sets.values.first().size
 )
 
 class MapSizeTestContext(
-    val empty: () -> PersistentMap<Any, DummyValue>,
+    val empty: () -> PersistentMap<Any, Any>,
     val key: (Int) -> Any
-)
-
-fun mapSizeTest(name: String, size: Int, test: MapSizeTestContext.() -> PersistentMap<Any, DummyValue>) = MapSizes(
-    name,
-    size,
-    hashMap = MapSizeTestContext(empty = { fakePersistentMapOf() }, key = { HashableSizeKey(it) }).test().computeOverhead(),
-    persistentHashMap = MapSizeTestContext(empty = { persistentHashMapOf() }, key = { HashableSizeKey(it) }).test().computeOverhead(),
-    hashTreapMap = MapSizeTestContext(empty = { treapMapOf() }, key = { HashableSizeKey(it) }).test().computeOverhead(),
-    sortedTreapMap = MapSizeTestContext(empty = { treapMapOf() }, key = { ComparableSizeKey(it) }).test().computeOverhead(),
-)
-
-val mapSizes = sequence {
-    yield(mapSizeTest("Empty", 0) { empty() })
-    (1..100).forEach {
-        yield(mapSizeTest("Fresh", it) { empty() + (1..it).map { key(it) to DummyValue } })
-    }
-}
-
-
-@Serializable
-data class SetSizes(
-    val scenario: String,
-    val size: Int,
-    val hashSet: Long,
-    val persistentHashSet: Long,
-    val hashTreapSet: Long,
-    val sortedTreapSet: Long,
 )
 
 class SetSizeTestContext(
@@ -63,39 +38,76 @@ class SetSizeTestContext(
     val key: (Int) -> Any
 )
 
-fun setSizeTest(name: String, size: Int, test: SetSizeTestContext.() -> PersistentSet<Any>) = SetSizes(
+fun map(name: String, test: MapSizeTestContext.() -> PersistentMap<Any, Any>) = Maps(
     name,
-    size,
-    hashSet = SetSizeTestContext(empty = { fakePersistentSetOf() }, key = { HashableSizeKey(it) }).test().computeOverhead(),
-    persistentHashSet = SetSizeTestContext(empty = { persistentHashSetOf() }, key = { HashableSizeKey(it) }).test().computeOverhead(),
-    hashTreapSet = SetSizeTestContext(empty = { treapSetOf() }, key = { HashableSizeKey(it) }).test().computeOverhead(),
-    sortedTreapSet = SetSizeTestContext(empty = { treapSetOf() }, key = { ComparableSizeKey(it) }).test().computeOverhead(),
+    mapOf(
+        "HashMap" to MapSizeTestContext(empty = { fakePersistentMapOf() }, key = { HashableSizeKey(it) }).test(),
+        "PersistentHashMap" to MapSizeTestContext(empty = { persistentHashMapOf() }, key = { HashableSizeKey(it) }).test(),
+        "HashTreapMap" to MapSizeTestContext(empty = { treapMapOf() }, key = { HashableSizeKey(it) }).test(),
+        "SortedTreapMap" to MapSizeTestContext(empty = { treapMapOf() }, key = { ComparableSizeKey(it) }).test(),
+    )
 )
 
-val setSizes = sequence {
-    yield(setSizeTest("Empty", 0) { empty() })
+fun set(name: String, test: SetSizeTestContext.() -> PersistentSet<Any>) = Sets(
+    name,
+    mapOf(
+        "HashSet" to SetSizeTestContext(empty = { fakePersistentSetOf() }, key = { HashableSizeKey(it) }).test(),
+        "PersistentHashSet" to SetSizeTestContext(empty = { persistentHashSetOf() }, key = { HashableSizeKey(it) }).test(),
+        "HashTreapSet" to SetSizeTestContext(empty = { treapSetOf() }, key = { HashableSizeKey(it) }).test(),
+        "SortedTreapSet" to SetSizeTestContext(empty = { treapSetOf() }, key = { ComparableSizeKey(it) }).test(),
+    )
+)
+
+val maps = sequence {
+    yield(map("Empty") { empty() })
     (1..100).forEach {
-        yield(setSizeTest("Fresh", it) { empty() + (1..it).map { key(it) } })
+        yield(map("Fresh") { empty() + (1..it).map { key(it) to DummyValue } })
     }
 }
 
-fun <K, V> Map<K, V>.computeOverhead(): Long {
+val sets = sequence {
+    yield(set("Empty") { empty() })
+    (1..100).forEach {
+        yield(set("Fresh") { empty() + (1..it).map { key(it) } })
+    }
+}
+
+fun <K, V> Map<K, V>.layout(): GraphLayout {
     if (this is FakePersistentMap<*, *>) {
-        return this.value.computeOverhead()
+        return this.value.layout()
     } else {
         val keysAndValues = GraphLayout.parseInstance(*(this.keys + this.values).toTypedArray())
-        return GraphLayout.parseInstance(this).subtract(keysAndValues).totalSize()
+        return GraphLayout.parseInstance(this).subtract(keysAndValues)
     }
 }
 
-fun <T> Set<T>.computeOverhead(): Long {
+fun <T> Set<T>.layout(): GraphLayout {
     if (this is FakePersistentSet<*>) {
-        return this.value.computeOverhead()
+        return this.value.layout()
     } else {
         val keysAndValues = GraphLayout.parseInstance(*(this.toList() as List<Any?>).toTypedArray())
-        return GraphLayout.parseInstance(this).subtract(keysAndValues).totalSize()
+        return GraphLayout.parseInstance(this).subtract(keysAndValues)
     }
 }
+
+fun GraphLayout.process(scenario: String, scenarioSize: Int, outputDir: Path): Long {
+    toImage(outputDir.resolve("$scenario.$scenarioSize.png").toString())
+    return totalSize()
+}
+
+@Serializable
+data class MapSizes(
+    val scenario: String,
+    val scenarioSize: Int,
+    val mapSizes: Map<String, Long>,
+)
+
+@Serializable
+data class SetSizes(
+    val scenario: String,
+    val scenarioSize: Int,
+    val setSizes: Map<String, Long>,
+)
 
 @Serializable
 data class Sizes(
@@ -103,15 +115,29 @@ data class Sizes(
     val maps: List<MapSizes>
 )
 
+
 /** Computes the sizes of various sets and maps, for comparison. Invoked by the `sizesBenchmark` Gradle task. */
 fun main(args: Array<String>) {
     val outputDir = Path.of(args[0])
-    Files.createDirectory(outputDir)
+    Files.createDirectories(outputDir)
 
     val sizes = Sizes(
-        sets = setSizes.toList(),
-        maps = mapSizes.toList()
+        sets = sets.map {
+            SetSizes(
+                scenario = it.scenario,
+                scenarioSize = it.scenarioSize,
+                setSizes = it.sets.mapValues { (_, set) -> set.layout().process(it.scenario, it.scenarioSize, outputDir) }
+            )
+        }.toList(),
+        maps = maps.map {
+            MapSizes(
+                scenario = it.scenario,
+                scenarioSize = it.scenarioSize,
+                mapSizes = it.maps.mapValues { (_, map) -> map.layout().process(it.scenario, it.scenarioSize, outputDir) }
+            )
+        }.toList()
     )
+
     val json = Json { prettyPrint = true }.encodeToString(sizes)
 
     Files.writeString(outputDir.resolve("sizes.json"), json)
